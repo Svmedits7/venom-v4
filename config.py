76351d69d265f62df57cd1d07eb891aea1d71f6c
Svmedits7 <mangalsawan1509@gmail.com>
2024-09-01 14:50:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7467612065:AAEG-n0rjIdcaS1CChTjWaxB5DZnqgrFtFk")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Svm7080")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5323258382")
 
 
 
